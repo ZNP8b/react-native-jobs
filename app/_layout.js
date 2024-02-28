d5b33102c2f8b8b37e6ff7dadfe64ce6a4686540
ZNP8b <znp8b00ff@gmail.com>
2024-02-28 19:40:48 +0300

    load fonts logic while splash screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,31 @@
 import { Stack } from "expo-router";
-import { useCallback } from "react";
+import { useEffect } from "react";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontError) throw error;
+  }, [fontError]);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 };
 
