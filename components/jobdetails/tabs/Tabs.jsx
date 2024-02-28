da9c270d7d6b97b16e8e295f9784372f751be4aa
ZNP8b <znp8b00ff@gmail.com>
2024-02-29 02:08:16 +0300

    get props

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -4,7 +4,7 @@ import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import styles from "./tabs.style";
 import { SIZES } from "../../../constants";
 
-const Tabs = () => {
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
     <View>
       <Text>Tabs</Text>
