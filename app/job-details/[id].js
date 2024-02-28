cb799c2a7551446ae35c73de55fc069f931d7d4d
ZNP8b <znp8b00ff@gmail.com>
2024-02-29 01:17:51 +0300

    react imports

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -7,6 +7,7 @@ import {
   RefreshControl,
 } from "react-native";
 import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
 
 const JobDetails = () => {
   return <Text>JobDetails</Text>;
