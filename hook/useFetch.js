13b25bf81a9053b7076b8c2a0661a4863605e050
ZNP8b <znp8b00ff@gmail.com>
2024-02-28 23:19:27 +0300

    options for fetching data

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = () => {
+const useFetch = (endpoint) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://65dd20e8e7edadead7ed6feb.mockapi.io/api/${endpoint}`,
+  };
 };
