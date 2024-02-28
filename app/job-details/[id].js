d3c6b6c20bd1f9767ad4590259ba71a2f888d1c9
ZNP8b <znp8b00ff@gmail.com>
2024-02-29 01:19:04 +0300

    local components import

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -9,6 +9,15 @@ import {
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
 
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+
 const JobDetails = () => {
   return <Text>JobDetails</Text>;
 };
