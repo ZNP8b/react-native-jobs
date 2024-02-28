dd86a739d8565fd15b67ad1fd158f178374ceb9f
ZNP8b <znp8b00ff@gmail.com>
2024-02-29 00:43:38 +0300

    import checkImageURL

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./popularjobcard.style";
+import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
